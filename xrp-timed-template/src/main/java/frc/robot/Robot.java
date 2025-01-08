a634261eb684bee23c9396685975e672629a9ee1
hen43 <whshle08@gmail.com>
2025-01-07 17:28:24 -0800

    controller sticks

--- xrp-timed-template/src/main/java/frc/robot/Robot.java
+++ xrp-timed-template/src/main/java/frc/robot/Robot.java
@@ -5,11 +5,15 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.xrp.XRPMotor;
 
 
 public class Robot extends TimedRobot {
+  private XRPMotor m_left = new XRPMotor(0);
+  private XRPMotor m_right = new XRPMotor(1);
+  private XboxController controller = new XboxController(0);
+
 
   public Robot() {}
 
@@ -26,11 +30,28 @@ public class Robot extends TimedRobot {
 
   /** This function is called once when teleop is enabled. */
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+    m_right.setInverted(true);
+  }
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    
+    /* WHY THE CONTROLLER STICKS ARE INVERTED
+    When viewed with each positive axis pointing toward you, counter-clockwise (CCW) is a positive value and clockwise (CW) is a negative value.
+    Because the controller is pointed away from you, the opposite is true.
+    Shifting the joystick forward is a counterclockwise turn with respect to the Y axis, so a negative value is returned.
+    Making the sticks negative are so up is up and down is down.
+    https://docs.wpilib.org/en/stable/docs/software/basic-programming/coordinate-system.html
+    */
+
+    double left_stick = -controller.getLeftY();
+    double right_stick = -controller.getRightY();
+
+    m_left.set(left_stick);
+    m_right.set(right_stick);
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
