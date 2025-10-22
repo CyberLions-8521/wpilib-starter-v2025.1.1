fdc73ae99c00c926340f9a90aeba5de48c40a0ed
ktvuwhs <ktvu@hbuhsd.edu>
2025-10-21 22:11:36 -0700

    added ps4 import

--- xrp-timed-template/src/main/java/frc/robot/Robot.java
+++ xrp-timed-template/src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.xrp.XRPGyro;
 import edu.wpi.first.wpilibj.xrp.XRPMotor;
 import edu.wpi.first.wpilibj.xrp.XRPOnBoardIO;
