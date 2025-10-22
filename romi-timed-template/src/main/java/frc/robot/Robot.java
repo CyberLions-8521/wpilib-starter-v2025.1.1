7e5adbe2908cbf820582ae615a85f4851d0b3ddc
ktvuwhs <ktvu@hbuhsd.edu>
2025-10-21 22:07:57 -0700

    initial commit

---
+++ romi-timed-template/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.romi.OnBoardIO;
+import edu.wpi.first.wpilibj.romi.OnBoardIO.ChannelMode;
+import edu.wpi.first.wpilibj.romi.RomiGyro;
+import edu.wpi.first.wpilibj.romi.RomiMotor;
+
+
+/**
+ * The methods in this class are called automatically corresponding to each mode, as described in
+ * the TimedRobot documentation. If you change the name of this class or the package after creating
+ * this project, you must also update the Main.java file in the project.
+ */
+public class Robot extends TimedRobot {
+ 
+  public Robot() {}
+
+  @Override
+  public void teleopInit()
+  {
+    // write code in here - runs once
+  }
+
+  @Override
+  public void teleopPeriodic()
+  {
+    // write code in here - runs forever
+  }
+
+
+
+
+
+
+
+
+
+
+  // Extra robot methods - uncomment to use
+  /*
+  @Override
+  public void robotPeriodic() {}
+  @Override
+  public void autonomousInit() {}
+  @Override
+  public void autonomousPeriodic() {}
+  @Override
+  public void disabledInit() {}
+  @Override
+  public void disabledPeriodic() {}
+  @Override
+  public void testInit() {}
+  @Override
+  public void testPeriodic() {}
+  */
+}
