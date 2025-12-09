0349d6326b811e8defd00d2210e9917f0baa08d2
ohnoruu <rubydao007@gmail.com>
2025-12-09 12:29:18 -0800

    Created new templates for motor workshop (no imports yet)

---
+++ kraken-timed-template/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+public class Robot extends TimedRobot {
+
+  public Robot() {
+   
+  }
+
+  @Override
+  public void teleopInit() {
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    
+  }
+}
---
+++ neo-timed-template/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,27 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  public Robot() {
+    
+  }
+
+
+  @Override
+  public void teleopInit() {
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+
+  }
+}
