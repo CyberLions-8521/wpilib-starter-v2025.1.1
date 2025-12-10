8971cacecc8f4708202b7655ce432d9c3ccaeb79
Kyrillos-Hanna <kyrohanna90@gmail.com>
2025-12-09 16:07:29 -0800

    added vendordeps and imports

--- kraken-timed-template/src/main/java/frc/robot/Robot.java
+++ kraken-timed-template/src/main/java/frc/robot/Robot.java
@@ -3,6 +3,9 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 
 public class Robot extends TimedRobot {
