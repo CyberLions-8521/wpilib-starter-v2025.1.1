b6fbf661db0cb1f7b77e173b0d7ce6b6b3c2eeaa
katerpi114r <kng100@student.hbuhsd.edu>
2025-12-17 02:45:22 +0000

    worked on robot

--- xrp-timed-template/src/main/java/frc/robot/Robot.java
+++ xrp-timed-template/src/main/java/frc/robot/Robot.java
@@ -6,6 +6,10 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 
+import javax.lang.model.util.ElementScanner14;
+
+import edu.wpi.first.wpilibj.Encoder;
+
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.PS4Controller;
@@ -19,19 +23,89 @@ import frc.robot.VexV5Controller;
 
 
 public class Robot extends TimedRobot {
+private XRPMotor LeftMotor = new XRPMotor(0); 
+private XRPMotor RightMotor = new XRPMotor(1); 
+private Encoder LeftEncoder = new Encoder(6,7);
+private Encoder RightEncoder = new Encoder(4,5);
 
-  public Robot() {}
+double WheelDiameter = 2.3622; 
+double TrackWith = 6.1;
+double PulsePerRev = 585;
+
+double circumference = Math.PI * WheelDiameter;
+double DistPerPulse = circumference/PulsePerRev;
+
+
+  public Robot() {
+    LeftEncoder.setDistancePerPulse(DistPerPulse);
+    RightEncoder.setDistancePerPulse(DistPerPulse);
+
+    RightMotor.setInverted(true);
+  }
 
   @Override
   public void teleopInit()
   {
+    resetEncoders();
     // type code in here - runs once
   }
 
+
+  //.setInverted(true)
   @Override
   public void teleopPeriodic()
   {
+    // LeftEncoder.getDistance();
+    // RightEncoder.getDistance();
+
+    if (getAverageDist() <= 10) {
+      moveForward();
+    }
+    else {
+      LeftMotor.set(0);
+      RightMotor.set(0);
+    }
+
+    // LeftMotor.set(0.5);
+    // RightMotor.set(-0.5); 
+
+
+    // if ((LeftEncoder.getDistance() + RightEncoder.getDistance()) / 2 > 10) {
+    //   LeftMotor.set(0.5);
+    //   RightMotor.set(0);
+    // }
+    // else {
+    //   LeftMotor.set(0);
+    //   RightMotor.set(0);
+    //   LeftEncoder.reset();
+    //   RightEncoder.reset(); 
+    // }
+
+    // if ((LeftEncoder.getDistance() + RightEncoder.getDistance()) / 2 <= 10) {
+    //   LeftMotor.set(0.5);
+    //   RightMotor.set(0.5);
+    // }
+    // else {
+    //   LeftMotor.set(0);
+    //   RightMotor.set(0);
+    // }
+
+
     // type code in here - runs forever
+  }
+
+  public void moveForward() {
+    LeftMotor.set(0.5);
+    RightMotor.set(0.5);
+  }
+
+  public double getAverageDist() {
+    return (LeftEncoder.getDistance() + RightEncoder.getDistance()) / 2;
+  }
+
+  public void resetEncoders() {
+    LeftEncoder.reset();
+    RightEncoder.reset(); 
   }
 
 
