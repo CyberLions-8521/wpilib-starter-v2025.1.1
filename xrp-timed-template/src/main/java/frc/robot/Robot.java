c54e77aff806927bce5471b9ea2d8de8866c739e
KevinOnATree <kon100@student.hbuhsd.edu>
2025-12-17 02:42:25 +0000

    worked on motor

--- xrp-timed-template/src/main/java/frc/robot/Robot.java
+++ xrp-timed-template/src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Encoder;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
@@ -19,19 +20,50 @@ import frc.robot.VexV5Controller;
 
 
 public class Robot extends TimedRobot {
+  private XRPMotor lmotor = new XRPMotor(0);
+  private XRPMotor rmotor = new XRPMotor(1);
+  private double speed = 0.5;
+  private Encoder leftEncoder = new Encoder(4,5);
+  private Encoder rightEncoder = new Encoder(6,7);
+  private double wheelDiameter = 2.3622;
+  private double trackWidth = 6.1;
+  private double pulsesPerRev = 585;
+  private double circumference = Math.PI * wheelDiameter;
+  private double distancePerPulse = circumference / pulsesPerRev;
 
-  public Robot() {}
-
+  public Robot() {
+    rmotor.setInverted(true);
+  }
+  public void moveFoward(double d) {
+    if ( ( (leftEncoder.getDistance() + rightEncoder.getDistance() ) / 2) > d) {
+      lmotor.stopMotor();
+      rmotor.stopMotor();
+  }
+  }
   @Override
   public void teleopInit()
   {
-    // type code in here - runs once
+    // type code in here - runs once\
+    leftEncoder.setDistancePerPulse(distancePerPulse);
+    rightEncoder.setDistancePerPulse(distancePerPulse);
+
+    leftEncoder.reset();
+    rightEncoder.reset();
   }
+
 
   @Override
   public void teleopPeriodic()
   {
-    // type code in here - runs forever
+    lmotor.set(speed);
+    rmotor.set(speed);
+    moveFoward(10);
+    lmotor.set(speed);
+    if (leftEncoder.getDistance() > 20) {
+      lmotor.stopMotor();
+    }
+
+
   }
 
 
